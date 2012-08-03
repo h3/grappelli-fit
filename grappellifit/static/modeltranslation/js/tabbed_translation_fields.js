02f85703d8dd7749117404bf33d526e7044f4707
h3 <haineault@gmail.com>
2012-08-02 22:32:53 -0300

    Update grappellifit/static/modeltranslation/js/tabbed_translation_fields.js
    Fixed bug in modeltranslation js

--- grappellifit/static/modeltranslation/js/tabbed_translation_fields.js
+++ grappellifit/static/modeltranslation/js/tabbed_translation_fields.js
@@ -115,72 +115,74 @@
                     else {
                         container = $('.group.tabular');
                     }
-                    tabs_container = $('<div class="modeltranslation-switcher-container"></div>');
-                    tabs_list = $('<ul class="modeltranslation-switcher"></ul>').appendTo(tabs_container);
-                    tabs_shim = $('<div style="display:none;" />').appendTo(tabs_container); // can't use real tabs, so we fake them
+                    if (container.length) {
+                        tabs_container = $('<div class="modeltranslation-switcher-container"></div>');
+                        tabs_list = $('<ul class="modeltranslation-switcher"></ul>').appendTo(tabs_container);
+                        tabs_shim = $('<div style="display:none;" />').appendTo(tabs_container); // can't use real tabs, so we fake them
 
-                    tabs_container.insertAfter(container.find('> .tools'));
-                    //tabs_container.appendTo(container.find('.thead'));
-            
-                    $.each(mt.languages, function (i, lang) {
-                        if (i == 0) {
-                            default_lang = lang;
-                        }
-                        var id = 'tab_'+ container.attr('id').replace('set-group', lang);
-                        $('<li><a href="#' + id + '">' + lang + '</a></li>')
-                            .appendTo(tabs_list)
-                            .find('a').bind('click', function() {
-                                var id = $(this).attr('href').replace('#', '');
-                                $.each(mt.languages, function (i, lang) {
-                                    var el = $('.'+ id.slice(0, -2) + lang)
-                                    if (id.slice(-2) != lang) {
-                                        el.hide();
-                                    }
-                                    else {
-                                        el.show();
-                                    }
+                        tabs_container.insertAfter(container.find('> .tools'));
+                        //tabs_container.appendTo(container.find('.thead'));
+                
+                        $.each(mt.languages, function (i, lang) {
+                            if (i == 0) {
+                                default_lang = lang;
+                            }
+                            var id = 'tab_'+ container.attr('id').replace('set-group', lang);
+                            $('<li><a href="#' + id + '">' + lang + '</a></li>')
+                                .appendTo(tabs_list)
+                                .find('a').bind('click', function() {
+                                    var id = $(this).attr('href').replace('#', '');
+                                    $.each(mt.languages, function (i, lang) {
+                                        var el = $('.'+ id.slice(0, -2) + lang)
+                                        if (id.slice(-2) != lang) {
+                                            el.hide();
+                                        }
+                                        else {
+                                            el.show();
+                                        }
+                                    });
+                                    return false;
                                 });
-                                return false;
-                            });
-                        $('<div id="'+ id +'" />').appendTo(tabs_shim);
-                    });
+                            $('<div id="'+ id +'" />').appendTo(tabs_shim);
+                        });
 
-                    container.find('.thead .th').each(function(i, th){
-                        th = $(th);
-                        var classes = th.attr('class').split(' ');
-                        // Remove language and brackets from field label, they are
-                        // displayed in the tab already.
-                        if (th.html()) {
-                            th.html(th.html().replace(/\ \[.+\]/, ''));
-                        }
-                        $.each(classes, function(x, classname){
-                            if ($.inArray(classname.slice(-2), mt.languages) > -1 && classname.slice(-3, -2) == '-') {
-                                var c = "tab_"+ container.attr('id').replace('set-group', classname.slice(-2));
-                                th.addClass(c);
-                                if (classname.slice(-2) != default_lang) {
-                                    th.hide();
+                        container.find('.thead .th').each(function(i, th){
+                            th = $(th);
+                            var classes = th.attr('class').split(' ');
+                            // Remove language and brackets from field label, they are
+                            // displayed in the tab already.
+                            if (th.html()) {
+                                th.html(th.html().replace(/\ \[.+\]/, ''));
+                            }
+                            $.each(classes, function(x, classname){
+                                if ($.inArray(classname.slice(-2), mt.languages) > -1 && classname.slice(-3, -2) == '-') {
+                                    var c = "tab_"+ container.attr('id').replace('set-group', classname.slice(-2));
+                                    th.addClass(c);
+                                    if (classname.slice(-2) != default_lang) {
+                                        th.hide();
+                                    }
                                 }
-                            }
-                        });
-                        
-                    });
-
-                    tabs_container.tabs();
-                    tabs.push(tabs_container);
-                    $.each(translations, function (name, languages) {
-                        $.each(languages, function(lang, el){
-                            var p = $(el).parent();
-                            var classname = container.attr('id').replace('set-group', lang);
-                            if (p.hasClass('td')) {
-                                p.addClass('tab_'+classname);
-                            }
-                            if (lang != default_lang) {
-                                p.hide();
-                            }
+                            });
+                            
                         });
 
-                    });
-                    return tabs;
+                        tabs_container.tabs();
+                        tabs.push(tabs_container);
+                        $.each(translations, function (name, languages) {
+                            $.each(languages, function(lang, el){
+                                var p = $(el).parent();
+                                var classname = container.attr('id').replace('set-group', lang);
+                                if (p.hasClass('td')) {
+                                    p.addClass('tab_'+classname);
+                                }
+                                if (lang != default_lang) {
+                                    p.hide();
+                                }
+                            });
+
+                        });
+                        return tabs;
+                    }
                 },
 
                 // Create change form tabbing
